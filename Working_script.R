a26df3163ef0f92e4027a3cdecced54360a4d224
rchavelas90 <rchavelas90@gmail.com>
2013-09-19 13:35:04 -0500

    Upload of files and function readGPX

---
+++ Working_script.R
@@ -0,0 +1,60 @@
+getwd()
+#Load readGPX Functions
+source("readGPX.R")
+#Verify that readGPX() and .readGPX.element() functions are loaded correctly
+ls(all.names=T)
+
+
+data <- readGPX("GPX_test_files/Tec_to_hm_2013-09-17T205421Z.gpx")
+setwd("GPX")
+
+str(data)
+str(data$tracks)
+data2 <- data$tracks[[1]][[1]]
+head(data2)
+head(data2$time)
+data2$delta <- c()
+data2$time <- as.POSIXct(data2$time, format="%Y-%m-%dT%H:%M:%SZ")
+plot(data2$lat,data2$lon)
+data2$loc=paste(data2$lat,data2$lon,sep=":")
+
+library(RgoogleMaps)
+MyMap <- MapBackground(lat=data2$lat,lon=data2$lon)
+PlotOnStaticMap(MyMap=MyMap,lat=data2$lat,lon=data2$lon,FUN=lines,destfile="ejm.png")
+
+
+library(googleVis)
+g <- gvisGeoChart(data2,"loc",options=list(region="MX"))
+plot(g)
+gvisGe
+
+head(data2)
+g2 <- gvisMap(data2,"loc",options=list(showLine=TRUE))
+plot(g2)
+
+data <- readGPX("2013-09-17-19-53.gpx",metadata=F,bounds=F,waypoints=F,route=F)
+setwd("GPX")
+
+str(data)
+str(data$tracks)
+data2 <- data$tracks[[1]][[1]]
+head(data2)
+head(data2$time)
+data2$delta <- c()
+data2$time <- as.POSIXct(data2$time, format="%Y-%m-%dT%H:%M:%SZ")
+plot(data2$lat,data2$lon)
+data2$loc=paste(data2$lat,data2$lon,sep=":")
+
+library(RgoogleMaps)
+MyMap <- MapBackground(lat=data2$lat,lon=data2$lon)
+PlotOnStaticMap(MyMap=MyMap,lat=data2$lat,lon=data2$lon,FUN=lines,destfile="ejm.png")
+
+
+library(googleVis)
+g <- gvisGeoChart(data2,"loc",options=list(region="MX"))
+plot(g)
+gvisGe
+
+head(data2)
+g2 <- gvisMap(data2,"loc",options=list(showLine=TRUE))
+plot(g2)
